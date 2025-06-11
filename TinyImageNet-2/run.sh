ff9169effc9adddae0060e55bd9c7421f26c1e61
RyougiSh1k1 <734714416@qq.com>
2025-06-11 16:34:57 +0000

    TinyImageNet

--- TinyImageNet-2/run.sh
+++ TinyImageNet-2/run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Run with 6 tasks, ~30 classes per task, and 1000 samples per task
 python main.py \
     --dataset=tiny_imagenet \
     --method=MFCL \
     --num_clients=100 \
     --n_tasks=6 \
+    --samples_per_task=1000 \
     --global_round=20 \
     --frac=0.1 \
     --lr=0.1 \
