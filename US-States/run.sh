4de86cec00b97aa9b4d78e240ca57e18a2d5bf77
RyougiSh1k1 <734714416@qq.com>
2025-06-20 19:27:56 +0000

    3

---
+++ US-States/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python main.py \
+    --dataset=tiny_imagenet \
+    --method=MFCL \
+    --num_clients=100 \
+    --n_tasks=6 \
+    --global_round=20 \
+    --frac=0.1 \
+    --lr=0.1 \
+    --batch_size=32 \
+    --seed=1 \
+    --path=data \
+    --epochs=50
